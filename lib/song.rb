d38ba69496040cea456f20f00245857ba22a50ee
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-10-04 01:18:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 class Song
-  attr_accessor :name, :artist_name, :song
+  attr_accessor :name, :artist_name
   @@all = []
 
   def self.all
@@ -10,98 +10,53 @@ class Song
     self.class.all << self
   end
 
-  def self.create(name = nil)
+  def self.create
     song = self.new
-    @@all << song
+    song.save
     song
   end
   
-  def self.new_by_name(name)
-    song = self.new
-    song.name = name
+  def self.new_by_name(song_name)
+    song = Song.create  # or instantiate and trigger .create method
+    song.name = song_name
     song
   end
   
-  def self.create_by_name(name = nil)
-    song = self.new
-    song.name = name
-    @@all << song
-    song
+  def self.create_by_name(song_name)
+    self.new_by_name(song_name)
   end
   
-  def self.find_by_name(name = nil)
-   self.all.find {|song| song.name == name}  
+  def self.find_by_name(song_name = nil)
+   self.all.find {|song| song.name == song_name}  
   end
   
- def self.find_or_create_by_name(name)
-  song1 = self.new
-  song2 = self.new
-  if song1.name == song2.name
-  find_by_name
-  else
-  create_by_name
-  end
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+    # if self.find_by_name(song_name)
+    # self.find_by_name(song_name)
+    # else
+    # self.create_by_name(song_name)
+    # end
   end
   
   def self.alphabetical
-    self.all.sort_by {|a, b| a <=> b}
+    self.all.sort_by {|song| song.name}
   end
   
   def self.new_from_filename(artist_and_song)
-   if artist_and_song.end_with?(".mp3")
-   song = self.new
-   data = artist_and_song.split("-")
-   song.artist_name = data[0]
-   song.name = data[1].chomp".mp3"
-   end
+   data = artist_and_song.split(" - ")
+   artist = data[0]
+   song_name = data[1].gsub(".mp3","") # - substitue first value with second, or data[1].split(".")[0] # data[1].gsub(".mp3",""), or chomp
+   song = self.new_by_name(song_name)
+   song.artist_name = artist
    song
   end
 
-  def self.create_from_filename(filename)
-  if filename.end_with?(".mp3")
-  filename.chomp ".mp3"
-  song = self.new
-  @@all << song
-  end
+  def self.create_from_filename(song)
+    self.new_from_filename(song)
   end
 
   def self.destroy_all
   self.all.clear
   end
-  
 end
-
-song = Song.create
-Song.all.include?(song) #=> true
-
-song = Song.new_by_name("The Middle")
-#=> #<Song @name="The Middle">
-song.name #=> "The Middle"
-
-song = Song.create_by_name("The Middle")
-#=> #<Song:0x007fd2a2989ff0 @name="The Middle">
-song
-#=> #<Song:0x007fd2a2989ff0 @name="The Middle">
-Song.all.include?(song)
-#=> true
-
-the_middle = Song.create_by_name("The Middle")
-#=> #<Song @name="The Middle">
-
-Song.find_by_name("The Middle")
-#<Song @name="The Middle">
-
-# song_1 = Song.find_or_create_by_name("Blank Space")
-# song_2 = Song.find_or_create_by_name("Blank Space")
-# # song_1 and song_2 are conceptually the same song and should return the same song instance because of `.find_or_create_by_name.`
-# song_1 == song_2 #=> true
-Song.alphabetical
-song = Song.new_from_filename("Taylor Swift - Blank Space.mp3")
-song.name #=> "Blank Space"
-song.artist_name #=> "Taylor Swift"
-
-song = Song.create_from_filename("Taylor Swift - Blank Space.mp3")
-Song.destroy_all
-
-
-
